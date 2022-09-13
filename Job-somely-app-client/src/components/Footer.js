c364e56fdba6f8c5f5fb6edc36aec14935ef1413
alikedo <alikemaldogan88@gmail.com>
2022-09-13 12:22:58 +0200

    modified Login,Signup pages

--- Job-somely-app-client/src/components/Footer.js
+++ Job-somely-app-client/src/components/Footer.js
@@ -1,10 +1,10 @@
 function Footer() {
 
   return (
-    <footer className="footer py-4 bg-opacity-90  mt-auto w-200 " style={{
-      backgroundColor: 'rgb(255, 225, 148)', color: 'rgb(46, 45, 64)', position: 'relative', bottom: '0'
+    <footer className="footer py-4   mt-auto w-200 " style={{
+      backgroundColor: 'rgb(255, 225, 148)', position: 'relative', bottom: '0'
     }}>
-      <div className="container text-center " >
+      <div className="container text-center fw-bold" style={{ color: 'rgb(46, 45, 64)' }} >
         <small>Created by Eurico Moutinho & Ali Kemal Dogan</small>
       </div>
     </footer >
--- Job-somely-app-client/src/pages/LoginPage.js
+++ Job-somely-app-client/src/pages/LoginPage.js
@@ -39,70 +39,72 @@ function LoginPage(props) {
     };
 
     return (
-        <section className="vh-100">
-            <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card shadow-lg" id="no-scale">
-                            <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <Image src="/job-somely-login.jpg" alt="login form" className="img-fluid h-100 rounded shadow-lg" />
-                                </div>
-                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
+        <div className="LoginPage" style={{ backgroundColor: 'rgb(234, 243, 248) ' }} >
+            <section className="vh-100">
+                <div className="container h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col col-xl-10">
+                            <div className="card shadow-lg" id="no-scale">
+                                <div className="row g-0" style={{ backgroundColor: 'rgb(255, 225, 148)' }}>
+                                    <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                        <Image src="/job-somely-login.jpg" alt="login form" className="img-fluid h-100 rounded shadow-lg" />
+                                    </div>
+                                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                        <div className="card-body p-4 p-lg-5 text-black">
 
-                                        <Form onSubmit={handleLoginSubmit}>
+                                            <Form onSubmit={handleLoginSubmit}>
 
-                                            <div className="feature-icon d-flex align-items-center mb-3 pb-1">
+                                                <div className="feature-icon d-flex align-items-center mb-3 pb-1">
 
-                                                <Image className="mx-auto" src="/job-somely-logo1.png" width={"100"} />
-                                            </div>
-
-
-                                            <h5 className="fw-normal mb-3 pb-3 text-success fw-bold">Sign into your account</h5>
-                                            {errorMessage && <p className="error-message text-danger fw-bold">{errorMessage}</p>}
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label">Username</label>
-                                                <input type="text"
-                                                    value={username}
-                                                    onChange={(e) => setUsername(e.target.value)} className="form-control form-control-lg"
-                                                    placeholder="nextjob" required />
+                                                    {/* <Image className="mx-auto" src="/job-somely-logo1.png" width={"100"} /> */}
+                                                </div>
 
 
+                                                <h5 className="fw-normal mb-3 pb-3  fw-bold" style={{ color: 'rgb(46, 45, 64)' }}>Sign into your account</h5>
+                                                {errorMessage && <p className="error-message text-danger fw-bold">{errorMessage}</p>}
 
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label">Password</label>
-                                                <input type="password"
-                                                    value={password}
-                                                    onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg"
-                                                    placeholder="********" required />
-
-
-                                            </div>
+                                                <div className="form-outline mb-4">
+                                                    <label className="form-label" style={{ color: 'rgb(46, 45, 64)' }}>Username</label>
+                                                    <input type="text"
+                                                        value={username}
+                                                        onChange={(e) => setUsername(e.target.value)} className="form-control form-control-lg"
+                                                        placeholder="nextjob" required />
 
 
-                                            <div className="pt-1 mb-4">
-                                                <Button className="bg-success btn-lg w-100" type="submit">Login</Button>
-                                            </div>
-                                            <p className="mb-5 pb-lg-2" >Don't have an account?
-                                                <NavLink to={"/login"}> Register here</NavLink></p>
-                                            <div>
-                                                <NavLink to="#!" className="small text-muted">Terms of use.</NavLink>
-                                                <NavLink to="#!" className="small text-muted">Privacy policy</NavLink>
-                                            </div>
-                                        </Form>
 
+                                                </div>
+
+                                                <div className="form-outline mb-4">
+                                                    <label className="form-label" style={{ color: 'rgb(46, 45, 64)' }}>Password</label>
+                                                    <input type="password"
+                                                        value={password}
+                                                        onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg"
+                                                        placeholder="********" required />
+
+
+                                                </div>
+
+
+                                                <div className="pt-1 mb-4">
+                                                    <Button className=" btn-lg w-100" type="submit" style={{ borderRadius: "40px", color: 'rgb(255, 225, 148)', border: "solid", backgroundColor: 'rgb(41, 52, 98)' }} >Login</Button>
+                                                </div>
+                                                <p className="mb-5 pb-lg-2" style={{ color: 'rgb(46, 45, 64)' }} >Don't have an account?
+                                                    <NavLink to={"/login"}> Register here</NavLink></p>
+                                                <div>
+                                                    <NavLink to="#!" className="small text-muted" style={{ color: 'rgb(46, 45, 64)' }}>Terms of use.</NavLink>
+                                                    <NavLink to="#!" className="small text-muted" style={{ color: 'rgb(46, 45, 64)' }}>Privacy policy</NavLink>
+                                                </div>
+                                            </Form>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
 
 
 
