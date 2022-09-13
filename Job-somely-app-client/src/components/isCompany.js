6dbbe03c1941bfd0708a8c57e4f76eed5ced20fd
Eurico Moutinho <yungsniper@hotmail>
2022-09-13 11:11:01 +0100

    With Route protection

--- Job-somely-app-client/src/components/isCompany.js
+++ Job-somely-app-client/src/components/isCompany.js
@@ -6,9 +6,11 @@ function IsCompany( { children } ) {
   
   const { isLoggedIn, user, isLoading } = useContext(AuthContext);
 
+  console.log(user)
+
   if (isLoading) return <p><img src={'https://c.tenor.com/y6RVjd7Dz8sAAAAC/loading-waiting.gif'}/></p>;
  
-  if (!isLoggedIn || user.type!=='company') {
+  if (!isLoggedIn || user.userType !== 'company') {
 
     return <Navigate to="/" />;
   } else {
